6a81df6e90359795b2d1581466a903a1e0d9f975
Per Henrik <phj@davidhorn.com>
2023-08-17 10:36:17 +0200

    feat(i3): add arandr scripts

---
+++ stow_home/arandr/.screenlayout/dell_office.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1 --primary --mode 3440x1440 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off
+feh --bg-fill ~/Pictures/Wallpapers/wp1.jpg
