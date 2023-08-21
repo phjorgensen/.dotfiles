c6c77777d207a491ea60670efdc112b26a91831c
Per Henrik <phj@davidhorn.com>
2023-08-21 07:30:51 +0200

    feat(polybar): try a config from github

--- stow_home/polybar/.config/polybar/launch.sh
+++ stow_home/polybar/.config/polybar/launch.sh
@@ -1,14 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
+
+MONITORS=$MONITORS polybar top &
+MONITOR=$MONITORS polybar bottom;
 
 echo "Bars launched..."
