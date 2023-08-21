d520f6bd2e0adfbabd9ef893949ce853932d4c84
Per Henrik <phj@davidhorn.com>
2023-08-21 07:32:20 +0200

    style(nvim): format config file

--- stow_home/nvim/.config/nvim/lua/phjorgensen/packer.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/packer.lua
@@ -29,17 +29,17 @@ return require('packer').startup(function(use)
 			-- LSP Support
 			{'neovim/nvim-lspconfig'},             -- Required
 			{                                      -- Optional
-			'williamboman/mason.nvim',
-			run = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+	  		'williamboman/mason.nvim',
+	  		run = function()
+	  			pcall(vim.cmd, 'MasonUpdate')
+	  		end,
+	  	},
+		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	};
-};
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    };
+  };
 end);
