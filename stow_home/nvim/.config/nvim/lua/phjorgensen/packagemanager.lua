fe31c79e0974add298d5e568e8f7df81b39e2be1
Per Henrik <phj@davidhorn.com>
2023-12-13 11:41:31 +0100

    chore(nvim): migrate to harpoon2

--- stow_home/nvim/.config/nvim/lua/phjorgensen/packagemanager.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/packagemanager.lua
@@ -35,7 +35,7 @@ require("lazy").setup({
   },
   {
     "ThePrimeagen/harpoon",
-    branch = "master",
+    branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
 	"ThePrimeagen/vim-be-good",
