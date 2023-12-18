8db62a70da27c66df9199b949948b69e17f5e380
Per Henrik <phj@davidhorn.com>
2023-12-18 07:13:54 +0100

    fix(harpoon): use list variable in select hotkeys

--- stow_home/nvim/.config/nvim/after/plugin/harpoon.lua
+++ stow_home/nvim/.config/nvim/after/plugin/harpoon.lua
@@ -11,13 +11,13 @@ vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(list, {
   ui_width_ratio = 0.5,
 }) end);
 
-vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end);
-vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end);
-vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end);
-vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end);
-vim.keymap.set("n", "<C-5>", function() harpoon:list():select(5) end);
-vim.keymap.set("n", "<C-6>", function() harpoon:list():select(6) end);
-vim.keymap.set("n", "<C-7>", function() harpoon:list():select(7) end);
-vim.keymap.set("n", "<C-8>", function() harpoon:list():select(8) end);
-vim.keymap.set("n", "<C-9>", function() harpoon:list():select(9) end);
-vim.keymap.set("n", "<C-0>", function() harpoon:list():select(10) end);
+vim.keymap.set("n", "<C-1>", function() list:select(1) end);
+vim.keymap.set("n", "<C-2>", function() list:select(2) end);
+vim.keymap.set("n", "<C-3>", function() list:select(3) end);
+vim.keymap.set("n", "<C-4>", function() list:select(4) end);
+vim.keymap.set("n", "<C-5>", function() list:select(5) end);
+vim.keymap.set("n", "<C-6>", function() list:select(6) end);
+vim.keymap.set("n", "<C-7>", function() list:select(7) end);
+vim.keymap.set("n", "<C-8>", function() list:select(8) end);
+vim.keymap.set("n", "<C-9>", function() list:select(9) end);
+vim.keymap.set("n", "<C-0>", function() list:select(10) end);
