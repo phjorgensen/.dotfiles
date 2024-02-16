684c713f711d3a9c5845ccfbd53700294f9801f0
Per <phjorgensenn@gmail.com>
2024-02-16 14:43:11 +0100

    feat(nvim): update colourscheme

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/colorscheme.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/colorscheme.lua
@@ -1,11 +1,25 @@
+-- Source: https://github.com/catppuccin/nvim
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd([[colorscheme rose-pine]])
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	end,
+  "catppuccin/nvim",
+  name = "catppuccin-mocha",
+  lazy = false,
+  priority = 1000,
+  config = function()
+	  vim.cmd([[colorscheme catppuccin]])
+	  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  end,
 }
+
+-- Source: https://github.com/rose-pine/neovim
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.cmd([[colorscheme rose-pine]])
+-- 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- 	end,
+-- }
