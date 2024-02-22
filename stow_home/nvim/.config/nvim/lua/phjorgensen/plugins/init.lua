e49fb9897e8e57cc7796a90f0e5ee0e410019c50
Per <phjorgensenn@gmail.com>
2024-02-22 09:31:30 +0100

    feat(tmux): update tmux config, add tpm and some packages

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/init.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/init.lua
@@ -1,8 +1,8 @@
 return {
-	"nvim-tree/nvim-web-devicons",
-	"nvim-lua/plenary.nvim",
-	"sbdchd/neoformat",
-	"mbbill/undotree",
-	"ThePrimeagen/vim-be-good",
-	"tpope/vim-fugitive",
+    "nvim-tree/nvim-web-devicons",
+    "nvim-lua/plenary.nvim",
+    "sbdchd/neoformat",
+    "mbbill/undotree",
+    "ThePrimeagen/vim-be-good",
+    "tpope/vim-fugitive",
 }
