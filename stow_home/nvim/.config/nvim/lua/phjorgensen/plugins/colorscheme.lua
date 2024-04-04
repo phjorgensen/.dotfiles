b2d6bb1e5a3b00cd422e7660d7686b93cae82579
Per <phjorgensenn@gmail.com>
2024-04-04 09:57:41 +0200

    feat(nvim): make sure we are using the mocha palette

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/colorscheme.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/colorscheme.lua
@@ -1,14 +1,18 @@
 -- Source: https://github.com/catppuccin/nvim
 return {
-  "catppuccin/nvim",
-  name = "catppuccin-mocha",
-  lazy = false,
-  priority = 1000,
-  config = function()
-	  vim.cmd([[colorscheme catppuccin]])
-	  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  end,
+    "catppuccin/nvim",
+    name = "catppuccin-mocha",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "mocha",
+        })
+
+        vim.cmd.colorscheme "catppuccin-mocha"
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end,
 }
 
 -- Source: https://github.com/rose-pine/neovim
