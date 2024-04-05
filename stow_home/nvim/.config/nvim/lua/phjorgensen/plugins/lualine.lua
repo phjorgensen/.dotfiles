8b6d82d65e0f10563f4dca41110d4566398d3485
Per <phjorgensenn@gmail.com>
2024-04-05 11:06:17 +0200

    feat: make terminal look better

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/lualine.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/lualine.lua
@@ -1,51 +1,59 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local lualine = require("lualine")
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local lualine = require("lualine")
+        local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
-		lualine.setup({
-			options = {
-				-- section_separators = { left = "", right = "󰁎" },
-				-- component_separators = { left = ">", right = "<" },
+        lualine.setup({
+            options = {
+                -- section_separators = { left = "", right = "󰁎" },
+                -- component_separators = { left = ">", right = "<" },
                 -- Available themes: https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
                 -- theme = "codedark",
                 theme = "catppuccin-mocha",
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = {
-					{ "filename", path = 1 },
-				},
-				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
-					},
-					"encoding",
-					"filesize",
-					"filetype",
-				},
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = {
-					{ "filename", path = 1 },
-				},
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
-			},
-			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
-			extensions = {},
-		})
-	end,
+                normal = {
+                    a = { bg = nil },
+                    b = { bg = nil },
+                    c = { bg = nil },
+                },
+                inactive = {
+                    a = { bg = nil },
+                    b = { bg = nil },
+                    c = { bg = nil },
+                },
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_c = {
+                    { "filename", path = 1 },
+                },
+                lualine_x = {
+                    {
+                        lazy_status.updates,
+                        cond = lazy_status.has_updates,
+                        color = { fg = "#ff9e64" },
+                    },
+                    "filetype",
+                },
+                lualine_y = { "progress" },
+                lualine_z = { "location" },
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {
+                    { "filename", path = 1 },
+                },
+                lualine_x = { "location" },
+                lualine_y = {},
+                lualine_z = {},
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {},
+        })
+    end,
 }
