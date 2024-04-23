c98d17f39fe5880404c7a3423a1fa8504465195d
Per <phjorgensenn@gmail.com>
2024-04-23 08:32:47 +0200

    style: format harpoon config

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/harpoon.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/harpoon.lua
@@ -1,33 +1,33 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    local harpoon = require("harpoon");
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        local harpoon = require("harpoon");
 
-    harpoon.setup();
+        harpoon.setup();
 
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end);
-    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end);
-    --  vim.keymap.set("n", "<C-e>", function()
-    --    harpoon.ui:toggle_quick_menu(harpoon:list(), {
-    --      title_pos = "center",
-    --      border = "double",
-    --      ui_width_ratio = 0.5,
-    --    })
-    --  end);
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end);
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end);
+        --  vim.keymap.set("n", "<C-e>", function()
+        --    harpoon.ui:toggle_quick_menu(harpoon:list(), {
+        --      title_pos = "center",
+        --      border = "double",
+        --      ui_width_ratio = 0.5,
+        --    })
+        --  end);
 
-    vim.keymap.set("n", "<C-1>", function() harpoon:list().select(1) end);
-    vim.keymap.set("n", "<C-2>", function() harpoon:list().select(2) end);
-    vim.keymap.set("n", "<C-3>", function() harpoon:list().select(3) end);
-    vim.keymap.set("n", "<C-4>", function() harpoon:list().select(4) end);
-    vim.keymap.set("n", "<C-5>", function() harpoon:list().select(5) end);
-    vim.keymap.set("n", "<C-6>", function() harpoon:list().select(6) end);
-    vim.keymap.set("n", "<C-7>", function() harpoon:list().select(7) end);
-    vim.keymap.set("n", "<C-8>", function() harpoon:list().select(8) end);
-    vim.keymap.set("n", "<C-9>", function() harpoon:list().select(9) end);
-    vim.keymap.set("n", "<C-0>", function() harpoon:list().select(10) end);
-  end,
+        vim.keymap.set("n", "<C-1>", function() harpoon:list().select(1) end);
+        vim.keymap.set("n", "<C-2>", function() harpoon:list().select(2) end);
+        vim.keymap.set("n", "<C-3>", function() harpoon:list().select(3) end);
+        vim.keymap.set("n", "<C-4>", function() harpoon:list().select(4) end);
+        vim.keymap.set("n", "<C-5>", function() harpoon:list().select(5) end);
+        vim.keymap.set("n", "<C-6>", function() harpoon:list().select(6) end);
+        vim.keymap.set("n", "<C-7>", function() harpoon:list().select(7) end);
+        vim.keymap.set("n", "<C-8>", function() harpoon:list().select(8) end);
+        vim.keymap.set("n", "<C-9>", function() harpoon:list().select(9) end);
+        vim.keymap.set("n", "<C-0>", function() harpoon:list().select(10) end);
+    end,
 }
