bad91188887afa872a5984085ff09ec935a15e7a
Per <phjorgensenn@gmail.com>
2024-04-24 07:58:06 +0200

    feat(nvim): replace fugitive with neogit

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/neogit.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/neogit.lua
@@ -1,9 +1,16 @@
 return {
     "NeogitOrg/neogit",
     dependencies = {
-        "nvim-lua/plenary.nvim",         -- required
-        "sindrets/diffview.nvim",        -- optional - Diff integration
-        "nvim-telescope/telescope.nvim", -- optional
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim",
     },
-    config = true
+    config = function()
+        local neogit = require("neogit");
+
+        neogit.setup();
+
+        vim.keymap.set("n", "<leader>gs", function() neogit.open() end);
+        vim.keymap.set("n", "<leader>gc", function() neogit.open({ "commit" }) end);
+    end
 }
