571b39fb4904497b2d523b8bd67cca8bccf492a2
Per <phjorgensenn@gmail.com>
2024-04-26 07:24:18 +0200

    feat(wezterm): change font to berkeley

--- stow_home/wezterm/.config/wezterm/wezterm.lua
+++ stow_home/wezterm/.config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local wezterm = require 'wezterm';
 -- This will hold the configuration.
 local config = wezterm.config_builder();
 
-config.font = wezterm.font 'MonaspaceRadon';
+config.font = wezterm.font 'BerkeleyMono';
 config.color_scheme = 'Catppuccin Mocha';
 
 config.window_background_opacity = 0.8;
