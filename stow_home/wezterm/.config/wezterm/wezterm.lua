ae4097ba8ad0ecf57b80607012a356bfdc78372c
Per <phjorgensenn@gmail.com>
2024-04-29 08:14:40 +0200

    feat(wezterm): turn off ligatures

--- stow_home/wezterm/.config/wezterm/wezterm.lua
+++ stow_home/wezterm/.config/wezterm/wezterm.lua
@@ -10,4 +10,6 @@ config.color_scheme = 'Catppuccin Mocha';
 config.window_background_opacity = 0.8;
 config.hide_tab_bar_if_only_one_tab = true;
 
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' };
+
 return config;
