ba66cacacab8d01595e2ab38bc4bce5a01c27a82
Per <phjorgensenn@gmail.com>
2024-05-27 13:59:49 +0200

    feat(nvim): add oil plugin

---
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/oil.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "stevearc/oil.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("oil").setup {
+                columns = { "icon" },
+                keymaps = {
+                    ["<CR>"] = "actions.select",
+                },
+                use_default_keymaps = false,
+                view_options = {
+                    show_hidden = true,
+                },
+            }
+
+            -- Open parent directory in current window
+            vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+        end,
+    },
+}
