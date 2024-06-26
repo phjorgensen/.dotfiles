02c1880184ab281e57f18d2330d9ab33c5027057
Per <phjorgensenn@gmail.com>
2024-06-26 08:49:39 +0200

    feat(nvim): update lazy bootstrapping

--- stow_home/nvim/.config/nvim/lua/phjorgensen/lazy.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/lazy.lua
@@ -1,21 +1,26 @@
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not vim.loop.fs_stat(lazypath) then
+---@diagnostic disable-next-line: undefined-field
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Add lazy to the `runtimepath`, this allows us to `require` it.
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("phjorgensen/plugins", {
+require("lazy").setup({
+  spec = {
+    { import = "phjorgensen/plugins" },
+  },
   install = {
     colorscheme = { "catppuccin" },
   },
