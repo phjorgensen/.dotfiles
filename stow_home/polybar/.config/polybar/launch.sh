f90f55a1696f452e1ea3fad2539b3c06f6ea4f5c
Per <phjorgensenn@gmail.com>
2024-08-01 22:03:14 +0200

    style: formatting

--- stow_home/polybar/.config/polybar/launch.sh
+++ stow_home/polybar/.config/polybar/launch.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
+# If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
 # Launch bars
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  for m in $(xrandr --query | grep " connected" | cut -d " " -f1); do
     MONITOR=$m polybar --reload main &
   done
 else
