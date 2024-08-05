fc1572d1dba98ea2d5769a65509ee0c5ea068023
Per <phj@davidhorn.com>
2024-08-05 11:08:18 +0200

    feat(telescope): include hidden files in search

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/telescope.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/telescope.lua
@@ -6,14 +6,30 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
+    local telescope = require("telescope")
+    telescope.setup({
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--hidden",
+        },
+      },
+    })
+
     local builtin = require("telescope.builtin")
 
     vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
     vim.keymap.set("n", "<C-p>", builtin.git_files, {})
     -- Not sure why ThePrimeagen added this instead of using live_grep, but keeping it in case I find out why.
-    -- vim.keymap.set('n', '<leader>ps', function()
-    -- 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-    -- end);
+    -- vim.keymap.set("n", "<leader>ps", function()
+    --   builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    -- end)
     vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>pr", builtin.resume, {})
   end,
