9d1ea895c1b58d31bb324f9465a34bcc65a404d0
Per <phjorgensenn@gmail.com>
2024-08-11 18:04:34 +0200

    feat(nvim): mason does not work with nix, use manual lsp

--- nix/modules/packages/development.nix
+++ nix/modules/packages/development.nix
@@ -7,8 +7,17 @@
     nodejs
     rustup
 
+    # Formatters
     stylua
     nodePackages.prettier
+
+    # LSPs
+    rust-analyzer
+    lua-language-server # lua_ls?
+    typescript
+    nodePackages.typescript-language-server # tsserver?
+    nodePackages.svelte-language-server # svelte?
+    nodePackages.tailwindcss # tailwindcss?
 
     postman
   ];
--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/lsp-zero.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/lsp-zero.lua
@@ -1,113 +1,158 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    -- LSP Support
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-
-    -- Autocompletion
-    { "hrsh7th/nvim-cmp" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "L3MON4D3/LuaSnip" },
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
   },
   config = function()
+    vim.opt.signcolumn = "yes"
+
+    local lspconfig = require("lspconfig")
+
+    lspconfig.util.default_config.capabilities = vim.tbl_deep_extend(
+      "force",
+      lspconfig.util.default_config.capabilities,
+      require("cmp_nvim_lsp").default_capabilities()
+    )
+
     local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    local default_setup = function(server_name)
-      require("lspconfig")[server_name].setup({
-        capabilities = lsp_capabilities,
-      })
-    end
-
-    require("mason").setup({})
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "rust_analyzer",
-        "eslint",
-        "tsserver",
-        "svelte",
-        "tailwindcss",
-      },
-      handlers = {
-        default_setup,
-        lua_ls = function()
-          require("lspconfig").lua_ls.setup({
-            capabilities = lsp_capabilities,
-            settings = {
-              Lua = {
-                runtime = {
-                  version = "LuaJIT",
-                },
-                diagnostics = {
-                  globals = { "vim" },
-                },
-                workspace = {
-                  library = {
-                    vim.env.VIMRUNTIME,
-                  },
-                },
-              },
+    require("lspconfig").lua_ls.setup({
+      capabilities = lsp_capabilities,
+      settings = {
+        Lua = {
+          runtime = {
+            version = "LuaJIT",
+          },
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            library = {
+              vim.env.VIMRUNTIME,
             },
-          })
-        end,
-        vtsls = function()
-          require("lspconfig").vtsls.setup({
-            capabilities = lsp_capabilities,
-            settings = {
-              javascript = {
-                preferences = {
-                  useAliasesForRenames = false,
-                  importModuleSpecifier = "non-relative",
-                },
-                suggestionActions = {
-                  enabled = false,
-                },
-              },
-              typescript = {
-                preferences = {
-                  useAliasesForRenames = false,
-                  importModuleSpecifier = "non-relative",
-                },
-                suggestionActions = {
-                  enabled = false,
-                },
-              },
-            },
-          })
-        end,
+          },
+        },
       },
     })
 
+    require("lspconfig").rust_analyzer.setup({})
+    require("lspconfig").eslint.setup({})
+    require("lspconfig").tsserver.setup({})
+    require("lspconfig").svelte.setup({})
+    require("lspconfig").tailwindcss.setup({})
+
     local cmp = require("cmp")
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
     cmp.setup({
       sources = {
         { name = "nvim_lsp" },
       },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-      }),
       snippet = {
         expand = function(args)
-          require("luasnip").lsp_expand(args.body)
+          vim.snippet.expand(args.body)
         end,
       },
+      mapping = cmp.mapping.preset.insert({}),
     })
 
-    vim.diagnostic.config({
-      float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = true,
-        header = "",
-        prefix = "",
-      },
-    })
+    -- local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    -- local default_setup = function(server_name)
+    --   require("lspconfig")[server_name].setup({
+    --     capabilities = lsp_capabilities,
+    --   })
+    -- end
+
+    -- require("mason").setup({})
+    -- require("mason-lspconfig").setup({
+    --   ensure_installed = {
+    --     "lua_ls",
+    --     "rust_analyzer",
+    --     "eslint",
+    --     "tsserver",
+    --     "svelte",
+    --     "tailwindcss",
+    --   },
+    --   handlers = {
+    --     default_setup,
+    --     lua_ls = function()
+    --       require("lspconfig").lua_ls.setup({
+    --         capabilities = lsp_capabilities,
+    --         settings = {
+    --           Lua = {
+    --             runtime = {
+    --               version = "LuaJIT",
+    --             },
+    --             diagnostics = {
+    --               globals = { "vim" },
+    --             },
+    --             workspace = {
+    --               library = {
+    --                 vim.env.VIMRUNTIME,
+    --               },
+    --             },
+    --           },
+    --         },
+    --       })
+    --     end,
+    --     vtsls = function()
+    --       require("lspconfig").vtsls.setup({
+    --         capabilities = lsp_capabilities,
+    --         settings = {
+    --           javascript = {
+    --             preferences = {
+    --               useAliasesForRenames = false,
+    --               importModuleSpecifier = "non-relative",
+    --             },
+    --             suggestionActions = {
+    --               enabled = false,
+    --             },
+    --           },
+    --           typescript = {
+    --             preferences = {
+    --               useAliasesForRenames = false,
+    --               importModuleSpecifier = "non-relative",
+    --             },
+    --             suggestionActions = {
+    --               enabled = false,
+    --             },
+    --           },
+    --         },
+    --       })
+    --     end,
+    --   },
+    -- })
+
+    -- local cmp = require("cmp")
+    -- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+    -- cmp.setup({
+    --   sources = {
+    --     { name = "nvim_lsp" },
+    --   },
+    --   mapping = cmp.mapping.preset.insert({
+    --     ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+    --     ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+    --     ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+    --     ["<C-Space>"] = cmp.mapping.complete(),
+    --   }),
+    --   snippet = {
+    --     expand = function(args)
+    --       require("luasnip").lsp_expand(args.body)
+    --     end,
+    --   },
+    -- })
+
+    -- vim.diagnostic.config({
+    --   float = {
+    --     focusable = false,
+    --     style = "minimal",
+    --     border = "rounded",
+    --     source = true,
+    --     header = "",
+    --     prefix = "",
+    --   },
+    -- })
   end,
 }
