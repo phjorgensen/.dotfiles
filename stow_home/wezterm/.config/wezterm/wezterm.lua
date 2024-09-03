e08133c5a6a9c6adad50e714b8a351668ac70276
Per <phjorgensenn@gmail.com>
2024-09-03 12:56:33 +0200

    style(wezterm): autoformat code

--- stow_home/wezterm/.config/wezterm/wezterm.lua
+++ stow_home/wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,15 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
-local config = wezterm.config_builder();
+local config = wezterm.config_builder()
 
-config.font = wezterm.font 'BerkeleyMono';
-config.color_scheme = 'Catppuccin Mocha';
+config.font = wezterm.font("BerkeleyMono")
+config.color_scheme = "Catppuccin Mocha"
 
-config.window_background_opacity = 0.8;
-config.hide_tab_bar_if_only_one_tab = true;
+config.window_background_opacity = 0.8
+config.hide_tab_bar_if_only_one_tab = true
 
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' };
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
-return config;
+return config
