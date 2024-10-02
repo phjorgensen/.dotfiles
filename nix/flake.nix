add6896f2c22485941edcc79a102266256e83cfb
phjorgensen <phjorgensenn@gmail.com>
2024-10-02 16:46:59 +0200

    feat(nix): add home-manager

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,6 +4,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     alejandra.url = "github:kamadorueda/alejandra/3.0.0";
     alejandra.inputs.nixpkgs.follows = "nixpkgs";
@@ -13,6 +15,7 @@
     self,
     nixpkgs,
     alejandra,
+    home-manager,
     ...
   } @ inputs: {
     system.autoUpgrade = {
@@ -36,6 +39,12 @@
 
         modules = [
           ./hosts/perCode/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.phj = import ../stow_home/home-manager/.config/home-manager/home.nix;
+          }
           {
             environment.systemPackages = [alejandra.defaultPackage.${system}];
           }
