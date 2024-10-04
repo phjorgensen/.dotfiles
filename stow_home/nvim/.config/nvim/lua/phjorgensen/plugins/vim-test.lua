b751fac31020c8a7f9f55164403ec4ccd75127c7
phjorgensen <phjorgensenn@gmail.com>
2024-10-04 22:27:39 +0200

    feat(nvim): remove unused plugins

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/vim-test.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/vim-test.lua
@@ -1,15 +1,15 @@
 return {
-  "vim-test/vim-test",
-  dependencies = {
-    "preservim/vimux",
-  },
-  config = function()
-    vim.keymap.set("n", "<leader>tn", "<cmd>:TestNearest<CR>")
-    vim.keymap.set("n", "<leader>tf", "<cmd>:TestFile<CR>")
-    vim.keymap.set("n", "<leader>ta", "<cmd>:TestSuite<CR>")
-    vim.keymap.set("n", "<leader>tl", "<cmd>:TestLast<CR>")
-    vim.keymap.set("n", "<leader>tv", "<cmd>:TestVisit<CR>")
-
-    vim.cmd("let test#strategy = 'vimux'")
-  end,
+  --   "vim-test/vim-test",
+  --   dependencies = {
+  --     "preservim/vimux",
+  --   },
+  --   config = function()
+  --     vim.keymap.set("n", "<leader>tn", "<cmd>:TestNearest<CR>")
+  --     vim.keymap.set("n", "<leader>tf", "<cmd>:TestFile<CR>")
+  --     vim.keymap.set("n", "<leader>ta", "<cmd>:TestSuite<CR>")
+  --     vim.keymap.set("n", "<leader>tl", "<cmd>:TestLast<CR>")
+  --     vim.keymap.set("n", "<leader>tv", "<cmd>:TestVisit<CR>")
+  --
+  --     vim.cmd("let test#strategy = 'vimux'")
+  --   end,
 }
