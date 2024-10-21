bd43bd0416f51cb6f97dc5297c4186c22ebea2ff
phjorgensen <phjorgensenn@gmail.com>
2024-10-21 17:57:47 +0200

    Add some mini.nvim plugins

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/mini.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/mini.lua
@@ -3,5 +3,18 @@ return {
   version = false,
   config = function()
     require("mini.ai").setup({})
+    require("mini.clue").setup({})
+    require("mini.comment").setup({})
+    require("mini.completion").setup({})
+    require("mini.git").setup({})
+    require("mini.hipatterns").setup({})
+    require("mini.icons").setup({})
+    require("mini.indentscope").setup({})
+    require("mini.notify").setup({})
+    require("mini.pairs").setup({})
+    require("mini.starter").setup({})
+    require("mini.statusline").setup({})
+    require("mini.surround").setup({})
+    require("mini.trailspace").setup({})
   end,
 }
