b4ea27cc19a6434257350a19eb514e3c7f6ba379
phjorgensen <phjorgensenn@gmail.com>
2024-11-01 15:16:36 +0100

    Update nix

--- stow_home/home-manager/.config/home-manager/flake.nix
+++ stow_home/home-manager/.config/home-manager/flake.nix
@@ -2,8 +2,8 @@
   description = "Home Manager configuration of phj";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
