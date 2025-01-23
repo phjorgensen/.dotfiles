7209bbfa84e011bafda0ec52d2fdee7094991239
phjorgensen <phjorgensenn@gmail.com>
2025-01-23 15:38:06 +0100

    Add ligatures to wezterm

--- stow_home/wezterm/.config/wezterm/wezterm.lua
+++ stow_home/wezterm/.config/wezterm/wezterm.lua
@@ -17,6 +17,6 @@ config.window_padding = {
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+-- config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 return config
