0a08642226788dd6507cba7f342558b6610d1c70
phjorgensen <phjorgensenn@gmail.com>
2025-01-23 20:51:34 +0100

    Get waybar from programs.waybar, install hyprlock

--- nix/modules/window-managers/hyprland.nix
+++ nix/modules/window-managers/hyprland.nix
@@ -1,10 +1,11 @@
 {pkgs, ...}: {
   programs.hyprland.enable = true;
+  programs.waybar.enable = true;
+  programs.hyprlock.enable = true;
 
-  environment.systemPackages = [
-    pkgs.kitty
-    pkgs.waybar
-    pkgs.hyprpaper
+  environment.systemPackages = with pkgs; [
+    kitty
+    hyprpaper
   ];
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
