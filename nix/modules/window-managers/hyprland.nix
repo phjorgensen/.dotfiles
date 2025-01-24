e366866082f87cf3ffac56816f1f157fd7e85eae
phjorgensen <phjorgensenn@gmail.com>
2025-01-24 08:37:20 +0100

    Configure hyprland some more

--- nix/modules/window-managers/hyprland.nix
+++ nix/modules/window-managers/hyprland.nix
@@ -3,9 +3,13 @@
   programs.waybar.enable = true;
   programs.hyprlock.enable = true;
 
+  services.hypridle.enable = true;
+
   environment.systemPackages = with pkgs; [
     kitty
     hyprpaper
+    xdg-desktop-portal-hyprland
+    hyprpolkitagent
   ];
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
