fb00657915b0b6f1416efcdcaf48ba8c7dd8e38e
phjorgensen <phjorgensenn@gmail.com>
2025-01-26 16:14:25 +0100

    Use UWSM with Hyprland, remove redundant packages

--- nix/modules/window-managers/hyprland.nix
+++ nix/modules/window-managers/hyprland.nix
@@ -1,15 +1,17 @@
 {pkgs, ...}: {
-  programs.hyprland.enable = true;
-  programs.waybar.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
+
   programs.hyprlock.enable = true;
+  programs.waybar.enable = true;
 
   services.hypridle.enable = true;
 
   environment.systemPackages = with pkgs; [
     kitty
     hyprpaper
-    xdg-desktop-portal-hyprland
-    hyprpolkitagent
   ];
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
