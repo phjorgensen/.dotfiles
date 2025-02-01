23f66b444ae4e534b61d0bd8e0b7d027e5885c75
phjorgensen <phjorgensenn@gmail.com>
2025-02-01 21:31:57 +0100

    Use greetd display-manager

--- nix/hosts/perCode/configuration.nix
+++ nix/hosts/perCode/configuration.nix
@@ -10,7 +10,7 @@
     ../../modules/enable-flakes.nix
     ../../modules/window-managers/i3.nix
     ../../modules/window-managers/hyprland.nix
-    ../../modules/display-managers/sddm.nix
+    ../../modules/display-managers/greetd.nix
     ../../modules/audio/pipewire.nix
     ../../modules/zsh.nix
     ../../modules/main-user.nix
