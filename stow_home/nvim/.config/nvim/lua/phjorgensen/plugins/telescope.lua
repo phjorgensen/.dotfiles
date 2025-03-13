d4df7deb5683f0b7c04e65da56aaf68550fb93dc
phjorgensen <phjorgensenn@gmail.com>
2025-03-13 21:13:12 +0100

    Add new binds to open telescope

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/telescope.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/telescope.lua
@@ -42,8 +42,14 @@ return {
 
     local builtin = require("telescope.builtin")
 
+    -- Trying out some new keybinds
+    vim.keymap.set("n", "<leader><leader>", builtin.find_files)
+    vim.keymap.set("n", "<leader>f", builtin.find_files)
+    vim.keymap.set("n", "<leader>/", builtin.live_grep)
+
     vim.keymap.set("n", "<leader>sa", builtin.find_files)
     vim.keymap.set("n", "<leader>sf", builtin.git_files)
+
     -- Not sure why ThePrimeagen added this instead of using live_grep, but keeping it in case I find out why.
     -- vim.keymap.set("n", "<leader>ps", function()
     --   builtin.grep_string({ search = vim.fn.input("Grep > ") })
