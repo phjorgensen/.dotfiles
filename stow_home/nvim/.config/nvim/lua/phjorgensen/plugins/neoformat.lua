cfa4bbd6c897084b995fb8dacf635c287f9fc2db
phjorgensen <phjorgensenn@gmail.com>
2025-03-13 21:12:01 +0100

    Turn on auto format, change format keybind

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/neoformat.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/neoformat.lua
@@ -3,13 +3,11 @@ return {
   config = function()
     vim.g.neoformat_try_node_exe = 1
 
-    vim.keymap.set({ "n", "x" }, "<leader>f", function()
-      vim.cmd("Neoformat")
-    end)
+    vim.keymap.set({ "n", "x" }, "<leader>cf", "<cmd>Neoformat<CR>")
 
-    -- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    --   pattern = { "*" },
-    --   command = "Neoformat",
-    -- })
+    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+      pattern = { "*" },
+      command = "Neoformat",
+    })
   end,
 }
