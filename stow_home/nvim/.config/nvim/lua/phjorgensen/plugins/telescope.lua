7e77abe807913d8fd4e61f9fc145501367495a86
phjorgensen <phjorgensenn@gmail.com>
2025-03-17 08:59:49 +0100

    Add description to some keybinds, to be displayed in which-key

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/telescope.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/telescope.lua
@@ -25,36 +25,23 @@ return {
         },
       },
       pickers = {
-        find_files = {
-          theme = "ivy",
-        },
-        git_files = {
-          theme = "ivy",
-        },
-        live_grep = {
-          theme = "ivy",
-        },
-        resume = {
-          theme = "ivy",
-        },
+        find_files = { theme = "ivy" },
+        git_files = { theme = "ivy" },
+        live_grep = { theme = "ivy" },
+        resume = { theme = "ivy" },
       },
     })
 
     local builtin = require("telescope.builtin")
 
     -- Trying out some new keybinds
-    vim.keymap.set("n", "<leader><leader>", builtin.git_files)
-    vim.keymap.set("n", "<leader>f", builtin.git_files)
-    vim.keymap.set("n", "<leader>/", builtin.live_grep)
+    vim.keymap.set("n", "<leader><leader>", builtin.git_files, { desc = "Search project files" })
+    vim.keymap.set("n", "<leader>f", builtin.git_files, { desc = "Search project files" })
+    vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Search for text in project files" })
 
-    vim.keymap.set("n", "<leader>sa", builtin.find_files)
-    vim.keymap.set("n", "<leader>sf", builtin.git_files)
-
-    -- Not sure why ThePrimeagen added this instead of using live_grep, but keeping it in case I find out why.
-    -- vim.keymap.set("n", "<leader>ps", function()
-    --   builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    -- end)
-    vim.keymap.set("n", "<leader>sg", builtin.live_grep)
-    vim.keymap.set("n", "<leader>sr", builtin.resume)
+    vim.keymap.set("n", "<leader>sa", builtin.find_files, { desc = "Search all project files" })
+    vim.keymap.set("n", "<leader>sf", builtin.git_files, { desc = "Search project files" })
+    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search for text in project files" })
+    vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "Resume search" })
   end,
 }
