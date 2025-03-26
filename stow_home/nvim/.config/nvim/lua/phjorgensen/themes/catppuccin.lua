266c07ee0577cf36e7ff7119113db956b563a916
phjorgensen <phjorgensenn@gmail.com>
2025-03-26 09:07:48 +0100

    Activate catppuccin theme for neovim

--- stow_home/nvim/.config/nvim/lua/phjorgensen/themes/catppuccin.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/themes/catppuccin.lua
@@ -5,10 +5,7 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    require("catppuccin").setup({
-      flavour = "mocha",
-    })
-
+    require("catppuccin").setup({ flavour = "mocha" })
     vim.cmd.colorscheme("catppuccin-mocha")
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- stow_home/nvim/.config/nvim/lua/phjorgensen/themes/disabled/rose-pine.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/themes/disabled/rose-pine.lua
@@ -5,7 +5,7 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd([[colorscheme rose-pine]])
+    vim.cmd.colorscheme("rose-pine")
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   end,
