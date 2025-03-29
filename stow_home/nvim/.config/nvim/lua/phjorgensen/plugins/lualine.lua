d4513f9802f68b903fbb617d095124cdeaa44d3a
phjorgensen <phjorgensenn@gmail.com>
2025-03-29 16:28:10 +0100

    Add lualine plugin to neovim

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/lualine.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/lualine.lua
@@ -28,7 +28,7 @@ return {
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
+            -- color = { fg = "#ff9e64" },
           },
           "filetype",
         },
