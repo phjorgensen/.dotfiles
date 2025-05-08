983342c7fb75f045f4d055bbd6bfc6d049b448ea
phjorgensen <phjorgensenn@gmail.com>
2025-05-08 09:13:14 +0200

    Remove unused flag from rofi-power

--- stow_home/hypr/.config/hypr/launch-rofi-power.sh
+++ stow_home/hypr/.config/hypr/launch-rofi-power.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rofi -show power -modes "power:~/.config/hypr/rofi-power.sh" -p "Power" -window-title "Power" -input "Test" -l 5 -i -theme-str "window {width: 12em;} listview {lines: 5;}"
+rofi -show power -modes "power:~/.config/hypr/rofi-power.sh" -p "Power" -window-title "Power" -l 5 -i -theme-str "window {width: 12em;} listview {lines: 5;}"
