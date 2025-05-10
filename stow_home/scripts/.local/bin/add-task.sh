739167dee2c0b54f7ead3fa52f966128b0f26be7
phjorgensen <phjorgensenn@gmail.com>
2025-05-10 18:41:43 +0200

    Check for empty input and existing files when creating a quick task
    Display a Zenity message box if there is no input or the file already
    exists.

--- stow_home/scripts/.local/bin/add-task.sh
+++ stow_home/scripts/.local/bin/add-task.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
 taskDescription=$(zenity --entry --title "Add new quick task" --width 500 --ok-label "Add" --text "Description")
-currentDate=$(date +"%Y-%m-%dT%H:%M:%S%:z")
+if [ -z "$taskDescription" ]; then
+  echo "No input was provided. A new file was not created."
+  zenity --warning --title "No input" --text "No input was provided.\nA new file was not created." --width 250
+  exit 0
+fi
+
 documentPath=~/Documents/notes/work/todo/"$taskDescription.md"
+if [ -f "$documentPath" ]; then
+  echo "A file with the same name already exists. A new file was not created."
+  zenity --error --title "File exists" --text "A file with the same name already exists.\nA new file was not created." --width 350
+  exit 0
+fi
+
+currentDate=$(date +"%Y-%m-%dT%H:%M:%S%:z")
 
 echo "---" >> "$documentPath"
 echo "createdAt: $currentDate" >> "$documentPath"
