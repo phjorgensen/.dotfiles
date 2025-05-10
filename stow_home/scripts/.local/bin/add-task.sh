1f5f8bcb28f06f4980add6990929e7ab5cca431b
phjorgensen <phjorgensenn@gmail.com>
2025-05-10 18:17:51 +0200

    Move task folder and improve dialog title

--- stow_home/scripts/.local/bin/add-task.sh
+++ stow_home/scripts/.local/bin/add-task.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-taskDescription=$(zenity --entry --title "Add new task" --width 500 --ok-label "Add" --text "Description")
+taskDescription=$(zenity --entry --title "Add new quick task" --width 500 --ok-label "Add" --text "Description")
 currentDate=$(date +"%Y-%m-%dT%H:%M:%S%:z")
-documentPath=~/Documents/notes/work/"$taskDescription.md"
+documentPath=~/Documents/notes/work/todo/"$taskDescription.md"
 
 echo "---" >> "$documentPath"
 echo "createdAt: $currentDate" >> "$documentPath"
