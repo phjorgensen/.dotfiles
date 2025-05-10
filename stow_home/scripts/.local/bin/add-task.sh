768dbbe8ea8406904de13f0203f082544dbbd33c
phjorgensen <phjorgensenn@gmail.com>
2025-05-10 19:01:05 +0200

    Add timestamp on conflicting file names, remove warning on no input
    No input will probably be intentional, don't need a warning for that.
    There will probably be conflicting file names at some point, instead of
    stopping it, I'll add a timestamp to the file name.

--- stow_home/scripts/.local/bin/add-task.sh
+++ stow_home/scripts/.local/bin/add-task.sh
@@ -3,15 +3,15 @@
 taskDescription=$(zenity --entry --title "Add new quick task" --width 500 --ok-label "Add" --text "Description")
 if [ -z "$taskDescription" ]; then
   echo "No input was provided. A new file was not created."
-  zenity --warning --title "No input" --text "No input was provided.\nA new file was not created." --width 250
   exit 0
 fi
 
 documentPath=~/Documents/notes/work/todo/"$taskDescription.md"
 if [ -f "$documentPath" ]; then
-  echo "A file with the same name already exists. A new file was not created."
-  zenity --error --title "File exists" --text "A file with the same name already exists.\nA new file was not created." --width 350
-  exit 0
+  echo "A file with the same name already exists. A new file was created with a timestamp."
+  zenity --warning --title "File exists" --text "A file with the same name already exists.\nA new file was created with a timestamp." --width 350
+  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  documentPath=~/Documents/notes/work/todo/"$taskDescription $timestamp.md"
 fi
 
 currentDate=$(date +"%Y-%m-%dT%H:%M:%S%:z")
