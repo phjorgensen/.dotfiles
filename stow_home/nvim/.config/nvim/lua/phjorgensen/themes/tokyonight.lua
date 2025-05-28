cd5abc57b50451d41b267dec9e2537450e748ec6
phjorgensen <phjorgensenn@gmail.com>
2025-05-28 15:29:57 +0200

    Try tokyonight

---
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/themes/tokyonight.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- require("tokyonight").setup({ style = "night" })
+    vim.cmd.colorscheme("tokyonight")
+    -- vim.cmd.colorscheme("tokyonight-night")
+    -- vim.cmd.colorscheme("tokyonight-storm")
+    -- vim.cmd.colorscheme("tokyonight-day")
+    -- vim.cmd.colorscheme("tokyonight-moon")
+  end,
+}
