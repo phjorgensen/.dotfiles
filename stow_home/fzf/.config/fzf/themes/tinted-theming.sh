1c889eaad34585f87e721062d18f3378d48d8865
phjorgensen <phjorgensenn@gmail.com>
2025-05-30 13:55:56 +0200

    Change to rebecca theme

--- stow_home/fzf/.config/fzf/themes/tinted-theming.sh
+++ stow_home/fzf/.config/fzf/themes/tinted-theming.sh
@@ -1,9 +1,9 @@
-# Scheme name: Zenburn
+# Scheme name: Rebecca
 # Scheme system: base16
-# Scheme author: elnawe
+# Scheme author: Victor Borja (http://github.com/vic) based on Rebecca Theme (http://github.com/vic/rebecca-theme)
 # Template author: Tinted Theming (https://github.com/tinted-theming)
 
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
-" --color=bg+:#404040,bg:#383838,spinner:#93e0e3,hl:#7cb8bb"\
-" --color=fg:#808080,header:#7cb8bb,info:#e0cf9f,pointer:#93e0e3"\
-" --color=marker:#93e0e3,fg+:#c0c0c0,prompt:#e0cf9f,hl+:#7cb8bb"
+" --color=bg+:#663399,bg:#292a44,spinner:#8eaee0,hl:#2de0a7"\
+" --color=fg:#a0a0c5,header:#2de0a7,info:#ae81ff,pointer:#8eaee0"\
+" --color=marker:#8eaee0,fg+:#ccccff,prompt:#ae81ff,hl+:#2de0a7"
