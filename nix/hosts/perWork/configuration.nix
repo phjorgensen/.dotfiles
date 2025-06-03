caaaf2414a241102c1dfeda3b2bc1f5fa3cb3f48
phjorgensen <phjorgensenn@gmail.com>
2025-06-03 13:31:22 +0200

    Add docker and podman to work computer

--- nix/hosts/perWork/configuration.nix
+++ nix/hosts/perWork/configuration.nix
@@ -17,7 +17,8 @@
     ../../modules/stylix/cursor-bibata.nix
     ../../modules/packages/general.nix
     ../../modules/packages/development.nix
-    # ../../modules/packages/docker.nix
+    ../../modules/packages/docker.nix
+    ../../modules/packages/podman.nix
     ../../modules/packages/media-editing.nix
     ../../modules/packages/security.nix
     ../../modules/packages/terminal.nix
