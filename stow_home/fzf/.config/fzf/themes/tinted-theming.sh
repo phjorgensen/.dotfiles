25bbbcf472dfb6ca59048814a080f2ae808c4ecb
phjorgensen <phjorgensenn@gmail.com>
2025-08-08 15:11:03 +0200

    Change to rebecca theme

--- stow_home/fzf/.config/fzf/themes/tinted-theming.sh
+++ stow_home/fzf/.config/fzf/themes/tinted-theming.sh
@@ -1,9 +1,9 @@
-# Scheme name: Everforest Dark Hard
+# Scheme name: Rebecca
 # Scheme system: base16
-# Scheme author: Sainnhe Park (https://github.com/sainnhe)
+# Scheme author: Victor Borja (http://github.com/vic) based on Rebecca Theme (http://github.com/vic/rebecca-theme)
 # Template author: Tinted Theming (https://github.com/tinted-theming)
 
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
-" --color=bg+:#2e383c,bg:#272e33,spinner:#83c092,hl:#7fbbb3"\
-" --color=fg:#9da9a0,header:#7fbbb3,info:#dbbc7f,pointer:#83c092"\
-" --color=marker:#83c092,fg+:#edeada,prompt:#dbbc7f,hl+:#7fbbb3"
+" --color=bg+:#663399,bg:#292a44,spinner:#8eaee0,hl:#2de0a7"\
+" --color=fg:#a0a0c5,header:#2de0a7,info:#ae81ff,pointer:#8eaee0"\
+" --color=marker:#8eaee0,fg+:#ccccff,prompt:#ae81ff,hl+:#2de0a7"
