a1eee7e6c6056c5d5e9d0beae9fb2abd7981989d
phjorgensen <phjorgensenn@gmail.com>
2025-08-11 15:12:03 +0200

    Change from rebecca to darkmoss theme

--- stow_home/fzf/.config/fzf/themes/tinted-theming.sh
+++ stow_home/fzf/.config/fzf/themes/tinted-theming.sh
@@ -1,9 +1,9 @@
-# Scheme name: Rebecca
+# Scheme name: darkmoss
 # Scheme system: base16
-# Scheme author: Victor Borja (http://github.com/vic) based on Rebecca Theme (http://github.com/vic/rebecca-theme)
+# Scheme author: Gabriel Avanzi (https://github.com/avanzzzi)
 # Template author: Tinted Theming (https://github.com/tinted-theming)
 
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
-" --color=bg+:#663399,bg:#292a44,spinner:#8eaee0,hl:#2de0a7"\
-" --color=fg:#a0a0c5,header:#2de0a7,info:#ae81ff,pointer:#8eaee0"\
-" --color=marker:#8eaee0,fg+:#ccccff,prompt:#ae81ff,hl+:#2de0a7"
+" --color=bg+:#252c2d,bg:#171e1f,spinner:#66d9ef,hl:#498091"\
+" --color=fg:#818f80,header:#498091,info:#fdb11f,pointer:#66d9ef"\
+" --color=marker:#66d9ef,fg+:#e3e3c8,prompt:#fdb11f,hl+:#498091"
