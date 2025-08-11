448f340a68104dbd01f8effc2c6414cc5513d496
phjorgensen <phjorgensenn@gmail.com>
2025-08-11 12:43:43 +0200

    Automatically display completion documentation in neovim

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/blink-cmp.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/blink-cmp.lua
@@ -1,14 +1,9 @@
 return {
   "saghen/blink.cmp",
-  -- optional: provides snippets for the snippet source
   dependencies = { "rafamadriz/friendly-snippets" },
 
   -- use a release tag to download pre-built binaries
   version = "*",
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
@@ -39,6 +34,22 @@ return {
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
+    },
+
+    signature = { enabled = true },
+
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500,
+      },
+      menu = {
+        auto_show = true,
+        draw = {
+          treesitter = { "lsp" },
+          columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
+        },
+      },
     },
 
     -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
