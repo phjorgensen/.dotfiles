1083e175672846b8efca72c4622d6f5fc36259eb
phjorgensen <phjorgensenn@gmail.com>
2025-08-15 08:12:17 +0200

    Update flake description to be more correct

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "The most awesomest NixOS configuration flake there is";
+  description = "The most awesomest NixOS configuration flake there ever was";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
