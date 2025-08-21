71e8e8f7f25a0227314522bd471de57916d82b8f
phjorgensen <phjorgensenn@gmail.com>
2025-08-21 10:04:05 +0200

    Change to Darkmoss

--- stow_home/fzf/.config/fzf/themes/tinted-theming.sh
+++ stow_home/fzf/.config/fzf/themes/tinted-theming.sh
@@ -1,9 +1,9 @@
-# Scheme name: Tomorrow Night
+# Scheme name: darkmoss
 # Scheme system: base16
-# Scheme author: Chris Kempson (http://chriskempson.com)
+# Scheme author: Gabriel Avanzi (https://github.com/avanzzzi)
 # Template author: Tinted Theming (https://github.com/tinted-theming)
 
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
-" --color=bg+:#282a2e,bg:#1d1f21,spinner:#8abeb7,hl:#81a2be"\
-" --color=fg:#b4b7b4,header:#81a2be,info:#f0c674,pointer:#8abeb7"\
-" --color=marker:#8abeb7,fg+:#e0e0e0,prompt:#f0c674,hl+:#81a2be"
+" --color=bg+:#252c2d,bg:#171e1f,spinner:#66d9ef,hl:#498091"\
+" --color=fg:#818f80,header:#498091,info:#fdb11f,pointer:#66d9ef"\
+" --color=marker:#66d9ef,fg+:#e3e3c8,prompt:#fdb11f,hl+:#498091"
