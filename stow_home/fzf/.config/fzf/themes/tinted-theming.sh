acdb9a942f3dbbb35404070bf2fe0f4629665a7c
phjorgensen <phjorgensenn@gmail.com>
2025-08-25 21:20:20 +0200

    Change to darkmoss

--- stow_home/fzf/.config/fzf/themes/tinted-theming.sh
+++ stow_home/fzf/.config/fzf/themes/tinted-theming.sh
@@ -1,9 +1,9 @@
-# Scheme name: Kanagawa
+# Scheme name: darkmoss
 # Scheme system: base16
-# Scheme author: Tommaso Laurenzi (https://github.com/rebelot)
+# Scheme author: Gabriel Avanzi (https://github.com/avanzzzi)
 # Template author: Tinted Theming (https://github.com/tinted-theming)
 
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
-" --color=bg+:#16161d,bg:#1f1f28,spinner:#6a9589,hl:#7e9cd8"\
-" --color=fg:#727169,header:#7e9cd8,info:#c0a36e,pointer:#6a9589"\
-" --color=marker:#6a9589,fg+:#c8c093,prompt:#c0a36e,hl+:#7e9cd8"
+" --color=bg+:#252c2d,bg:#171e1f,spinner:#66d9ef,hl:#498091"\
+" --color=fg:#818f80,header:#498091,info:#fdb11f,pointer:#66d9ef"\
+" --color=marker:#66d9ef,fg+:#e3e3c8,prompt:#fdb11f,hl+:#498091"
