1a8b2d64046f5a35d116c978a1a7aa3d124d0c16
phjorgensen <phjorgensenn@gmail.com>
2025-08-27 08:51:55 +0200

    Add, commented out, override for teams to update version

--- nix/modules/packages/work.nix
+++ nix/modules/packages/work.nix
@@ -1,5 +1,26 @@
 { inputs, pkgs, ... }:
+let
+  # teams-for-linux = pkgs.teams-for-linux.overrideAttrs (oldAttrs: rec {
+  #   pname = "teams-for-linux";
+  #   version = "2.5.1";
+
+  #   src = pkgs.fetchFromGitHub {
+  #     owner = "IsmaelMartinez";
+  #     repo = "teams-for-linux";
+  #     tag = "v${version}";
+  #     hash = "sha256-yjMhWP/AtjFKhbvSR4RzYD6vedc+OgqPU7bTWrNqtmo=";
+  #   };
+
+  #   npmDepsHash = "sha256-wnr1MO0DN+2rKjmM75V3m1oOVczfDslOtULn/tLbRzM=";
+  #   npmDeps = pkgs.fetchNpmDeps {
+  #     inherit src;
+  #     name = "${pname}-${version}-npm-deps";
+  #     hash = npmDepsHash;
+  #   };
+  # });
+in
 {
+
   environment.systemPackages = with pkgs; [
     slack
     gitkraken
