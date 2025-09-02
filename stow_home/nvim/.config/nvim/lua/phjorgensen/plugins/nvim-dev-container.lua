1268073ba43b8e3f639a3f858b65edc5806691f1
phjorgensen <phjorgensenn@gmail.com>
2025-09-02 14:26:17 +0200

    Add devcontainer plugin to nvim

---
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/nvim-dev-container.lua
@@ -0,0 +1,19 @@
+return {
+  "https://codeberg.org/esensar/nvim-dev-container",
+  dependencies = "nvim-treesitter/nvim-treesitter",
+  config = function()
+    require("devcontainer").setup({})
+
+    vim.keymap.set("n", "<leader>os", "<cmd>DevcontainerStart<CR>", { desc = "Start devcontainer" })
+    vim.keymap.set("n", "<leader>oa", "<cmd>DevcontainerAttach<CR>", { desc = "Attatch to devcontainer" })
+    vim.keymap.set("n", "<leader>oe", "<cmd>DevcontainerExec ", { desc = "Exec command on devcontainer" })
+    vim.keymap.set("n", "<leader>oS", "<cmd>DevcontainerStop<CR>", { desc = "Stop devcontainer" })
+    vim.keymap.set("n", "<leader>ol", "<cmd>DevcontainerLogs<CR>", { desc = "Open devcontainer log file" })
+    vim.keymap.set(
+      "n",
+      "<leader>oc",
+      "<cmd>DevcontainerEditNearestConfig<CR>",
+      { desc = "Edit nearest devcontainer config" }
+    )
+  end,
+}
