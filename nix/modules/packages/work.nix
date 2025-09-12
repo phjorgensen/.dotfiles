d099b9a755675ca56c4ea8309e48ba9d3bc4c22e
phjorgensen <phjorgensenn@gmail.com>
2025-09-12 16:03:23 +0200

    Override teams-for-linux with v2.1.0, the broken has made it to stable

--- nix/modules/packages/work.nix
+++ nix/modules/packages/work.nix
@@ -1,23 +1,26 @@
 { inputs, pkgs, ... }:
 let
-  # teams-for-linux = pkgs.teams-for-linux.overrideAttrs (oldAttrs: rec {
-  #   pname = "teams-for-linux";
-  #   version = "2.5.1";
+  teams-for-linux = pkgs.teams-for-linux.overrideAttrs (oldAttrs: rec {
+    pname = "teams-for-linux";
+    version = "2.1.0";
 
-  #   src = pkgs.fetchFromGitHub {
-  #     owner = "IsmaelMartinez";
-  #     repo = "teams-for-linux";
-  #     tag = "v${version}";
-  #     hash = "sha256-yjMhWP/AtjFKhbvSR4RzYD6vedc+OgqPU7bTWrNqtmo=";
-  #   };
+    src = pkgs.fetchFromGitHub {
+      owner = "IsmaelMartinez";
+      repo = "teams-for-linux";
+      tag = "v${version}";
+      hash = "sha256-lISDy721e3bfWMl56DlIxVKN2bW8Yonc5XSVL072OQk="; # v2.1.0
+      # hash = "sha256-yjMhWP/AtjFKhbvSR4RzYD6vedc+OgqPU7bTWrNqtmo="; # v2.5.1
+    };
 
-  #   npmDepsHash = "sha256-wnr1MO0DN+2rKjmM75V3m1oOVczfDslOtULn/tLbRzM=";
-  #   npmDeps = pkgs.fetchNpmDeps {
-  #     inherit src;
-  #     name = "${pname}-${version}-npm-deps";
-  #     hash = npmDepsHash;
-  #   };
-  # });
+    npmDepsHash = "sha256-QcjXJcEIi/sUJLUF+wMqhXyLYPgjZKK6n4ngyvrH9NA="; # v2.1.0
+    # npmDepsHash = "sha256-wnr1MO0DN+2rKjmM75V3m1oOVczfDslOtULn/tLbRzM="; # v2.5.1
+
+    npmDeps = pkgs.fetchNpmDeps {
+      inherit src;
+      name = "${pname}-${version}-npm-deps";
+      hash = npmDepsHash;
+    };
+  });
 in
 {
 
@@ -26,8 +29,8 @@ in
     gitkraken
     thunderbird-128
 
-    # teams-for-linux
-    inputs.stable-nixpkgs.legacyPackages.${pkgs.system}.teams-for-linux
+    teams-for-linux
+    # inputs.stable-nixpkgs.legacyPackages.${pkgs.system}.teams-for-linux
   ];
 
   services.flatpak.enable = true;
