612e6f0672bf17bbe79f03cdf2a61b7a587b9df8
phjorgensen <phjorgensenn@gmail.com>
2025-09-24 22:15:21 +0200

    Update the Niri config

--- nix/modules/window-managers/niri.nix
+++ nix/modules/window-managers/niri.nix
@@ -1,34 +1,45 @@
 { pkgs, ... }:
 {
+  imports = [
+    ../../modules/audio/pipewire.nix
+  ];
+
   programs.niri = {
     enable = true;
   };
 
+  security.polkit.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.swaylock = { };
   programs.waybar.enable = true;
 
   # services.hypridle.enable = true;
 
   environment.systemPackages = with pkgs; [
-    # Authentication agent
-    hyprpolkitagent
+    ghostty
+    # rofi
+    fuzzel
+
+    swaylock
+    swayidle
+
+    # Nofification daemon
+    # swaynotificationcenter
+    mako
 
     # Wallpaper manager
     swww
 
-    # Lock screen
-    swaylock
-
-    # Nofification daemon
-    swaynotificationcenter
-
-    # App launcher
-    rofi
-    fuzzel
+    xdg-desktop-portal
+    xdg-desktop-portal-wlr
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-gnome
   ];
 
   qt.enable = true;
 
   # Tell apps to use wayland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.ELECTRON_OZONE_PLATFORM_HINT = "auto";
   environment.sessionVariables.MOZ_ENABLE_WAYLAND = "1";
 }
