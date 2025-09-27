03fa86d91642d9b6a12d4ae47e4034fdb03a0e13
phjorgensen <phjorgensenn@gmail.com>
2025-09-27 17:08:59 +0200

    Clean up a module

--- nix/modules/security.nix
+++ nix/modules/security.nix
@@ -1,9 +1,6 @@
 { ... }:
 {
-  # services.yubikey-agent.enable = true; # SSH agent for YubiKey, will overwrite the Gnome Keyring
   programs.yubikey-touch-detector.enable = true;
-
-  services = {
-    gnome.gnome-keyring.enable = true;
-  };
+  # services.yubikey-agent.enable = true; # SSH agent for YubiKey, will overwrite the Gnome Keyring
+  services.gnome.gnome-keyring.enable = true;
 }
