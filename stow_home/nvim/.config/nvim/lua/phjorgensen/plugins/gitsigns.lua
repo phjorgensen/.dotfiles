695b5ac58d0280928d07b5f671c2bb2298681c32
phjorgensen <phjorgensenn@gmail.com>
2025-10-24 23:14:42 +0200

    Remove delay on gitsigns

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/gitsigns.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/gitsigns.lua
@@ -23,7 +23,7 @@ return {
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
+        delay = 0,
         ignore_whitespace = false,
         virt_text_priority = 100,
       },
