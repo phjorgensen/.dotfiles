d11c2b49fd35b3a15799492c016f3754f118eea8
phjorgensen <phjorgensenn@gmail.com>
2025-11-07 15:33:25 +0100

    Try to fix errors with highlight in nvim

--- stow_home/nvim/.config/nvim/lua/phjorgensen/init.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/init.lua
@@ -8,6 +8,26 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
+local links = {
+  ["@lsp.type.namespace"] = "@namespace",
+  ["@lsp.type.type"] = "@type",
+  ["@lsp.type.class"] = "@type",
+  ["@lsp.type.enum"] = "@type",
+  ["@lsp.type.interface"] = "@type",
+  ["@lsp.type.struct"] = "@structure",
+  ["@lsp.type.parameter"] = "@parameter",
+  ["@lsp.type.variable"] = "@variable",
+  ["@lsp.type.property"] = "@property",
+  ["@lsp.type.enumMember"] = "@constant",
+  ["@lsp.type.function"] = "@function",
+  ["@lsp.type.method"] = "@method",
+  ["@lsp.type.macro"] = "@macro",
+  ["@lsp.type.decorator"] = "@function",
+}
+for newgroup, oldgroup in pairs(links) do
+  vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+end
+
 -- Set transparent background
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
