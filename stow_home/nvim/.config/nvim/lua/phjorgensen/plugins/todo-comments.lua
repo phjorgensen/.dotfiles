64b16b70d89a7bbe9781e1af4d949d2c89afade3
phjorgensen <phjorgensenn@gmail.com>
2025-11-11 13:04:39 +0100

    Add keybinds for opening todo comments in Snacks.picker

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/todo-comments.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/todo-comments.lua
@@ -2,6 +2,23 @@ return {
   "folke/todo-comments.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "folke/snacks.nvim",
+  },
+  keys = {
+    {
+      "<leader>st",
+      function()
+        Snacks.picker.todo_comments()
+      end,
+      desc = "Todo",
+    },
+    {
+      "<leader>sT",
+      function()
+        Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+      end,
+      desc = "Todo/Fix/Fixme",
+    },
   },
   opts = {},
 }
