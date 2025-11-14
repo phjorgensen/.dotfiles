b523821b70238cab4a5dba5dcf46e7dc21747f82
phjorgensen <phjorgensenn@gmail.com>
2025-11-14 15:01:05 +0100

    Start on improvements to update script

--- rebuild_nix.sh
+++ rebuild_nix.sh
@@ -3,9 +3,15 @@
 cd nix
 
 skip_update_flag="--skip-update"
+skip_stow_flag="--skip-stow"
 
 if [[ $2 != $skip_update_flag ]]; then
   nix flake update
 fi
+
+# Not ready yet, need some check if args includes $skip_stow_flag or $skip_update_flag
+# if [[ $3 != $skip_stow_flag ]]; then
+#   ./run_stow_home.sh
+# fi
 
 sudo nixos-rebuild switch --flake ~/.dotfiles/nix#$1
