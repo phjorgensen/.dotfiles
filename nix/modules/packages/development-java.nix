bf3012f464a9dc451ac7cc9552bc591316458dd6
phjorgensen <phjorgensenn@gmail.com>
2026-01-18 21:11:39 +0100

    Use Eclipse Temurin JDK 21, rather than just OpenJDK 21

--- nix/modules/packages/development-java.nix
+++ nix/modules/packages/development-java.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    jdk21_headless
+    # jdk21_headless
+    javaPackages.compiler.temurin-bin.jdk-21
 
     maven # Java build tool
     # gradle_9 Java build tool
