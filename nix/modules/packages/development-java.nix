84eeb0fab6976bd5333c6e8cf5b74dad58377dff
phjorgensen <phjorgensenn@gmail.com>
2026-02-20 09:23:13 +0100

    Add gradle, as well as maven

--- nix/modules/packages/development-java.nix
+++ nix/modules/packages/development-java.nix
@@ -5,7 +5,7 @@
     javaPackages.compiler.temurin-bin.jdk-21
 
     maven # Java build tool
-    # gradle_9 Java build tool
+    gradle_9 # Java build tool
 
     # java-language-server
     jdt-language-server # Eclipse version of the LSP
