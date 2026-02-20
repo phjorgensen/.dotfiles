c275d2d50e7516d5952e4c4b89b4aa8e34517f57
phjorgensen <phjorgensenn@gmail.com>
2026-02-20 09:23:39 +0100

    Add some more build tools to the kotlin config

--- nix/modules/packages/development-kotlin.nix
+++ nix/modules/packages/development-kotlin.nix
@@ -1,7 +1,18 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
+    kotlin
+    ktfmt
+    ktlint
+    detekt
+
     jetbrains.idea-oss
+    # jetbrains.idea # Non-free
+
+    javaPackages.compiler.temurin-bin.jdk-21
+
+    maven # Java build tool
+    gradle_9 # Java build tool
 
     # Unofficial and deprecated, change to [kotlin-lsp](https://github.com/Kotlin/kotlin-lsp) when it's more stable (not yet in nixpkgs either)
     kotlin-language-server
