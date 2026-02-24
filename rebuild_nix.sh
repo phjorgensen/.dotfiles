c1454ff36dfae2f4f08ff823eaafc23d80242057
phjorgensen <phjorgensenn@gmail.com>
2026-02-24 09:41:59 +0100

    Add feedback for each stage in Nix update script

--- rebuild_nix.sh
+++ rebuild_nix.sh
@@ -5,12 +5,12 @@ SKIP_STOW_FLAG="--skip-stow"
 BOOT_FLAG="--with-boot"
 
 args="$@"
-echo "Args: $args"
 
 if [[ ! " ${args[*]} " =~ [[:space:]]${SKIP_STOW_FLAG}[[:space:]] ]];
 then
-  echo "Running stow"
+  echo "Running stow..."
   ./run_stow_home.sh
+  echo "Done!"
 else
   echo "Skipping stow"
 fi
@@ -19,17 +19,20 @@ cd nix
 
 if [[ ! " ${args[*]} " =~ [[:space:]]${SKIP_UPDATE_FLAG}[[:space:]] ]];
 then
-  echo "Updating flake"
+  echo "Updating flake..."
   nix flake update
+  echo "Done!"
 else
   echo "Skipping flake update"
 fi
 
 if [[ " ${args[*]} " =~ [[:space:]]${BOOT_FLAG}[[:space:]] ]];
 then
-  echo "Running nixos-rebuild boot"
+  echo "Running nixos-rebuild boot..."
   sudo nixos-rebuild boot --flake ~/.dotfiles/nix#perWork
+  echo "Done!"
 else
-  echo "Running nixos-rebuild switch"
+  echo "Running nixos-rebuild switch..."
   sudo nixos-rebuild switch --flake ~/.dotfiles/nix#$1
+  echo "Done!"
 fi
