6839127c77653678785db8d296c60b9c27a8afd2
phjorgensen <phjorgensenn@gmail.com>
2026-03-18 12:30:01 +0100

    Unstable teams-for-linux back in business

--- nix/modules/packages/work.nix
+++ nix/modules/packages/work.nix
@@ -1,11 +1,10 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
     slack
     gitkraken
     thunderbird
-    # teams-for-linux # Beaks the build, use stable for now
-    inputs.stable-nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.teams-for-linux
+    teams-for-linux
   ];
 
   services.flatpak.enable = true;
