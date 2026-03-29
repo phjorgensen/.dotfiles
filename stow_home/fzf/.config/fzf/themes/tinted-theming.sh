0c0b48074dde0ac91cf1f95215069f58cb76eacc
phjorgensen <phjorgensenn@gmail.com>
2026-03-30 00:22:55 +0200

    Set theme to Catppuccin Mocha

--- stow_home/fzf/.config/fzf/themes/tinted-theming.sh
+++ stow_home/fzf/.config/fzf/themes/tinted-theming.sh
@@ -1,9 +1,9 @@
-# Scheme name: Monokai
+# Scheme name: Catppuccin Mocha
 # Scheme system: base16
-# Scheme author: Wimer Hazenberg (http://www.monokai.nl)
+# Scheme author: https://github.com/catppuccin/catppuccin
 # Template author: Tinted Theming (https://github.com/tinted-theming)
 
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
-" --color=bg+:#383830,bg:#272822,spinner:#a1efe4,hl:#66d9ef"\
-" --color=fg:#a59f85,header:#66d9ef,info:#f4bf75,pointer:#a1efe4"\
-" --color=marker:#a1efe4,fg+:#f5f4f1,prompt:#f4bf75,hl+:#66d9ef"
+" --color=bg+:#181825,bg:#1e1e2e,spinner:#94e2d5,hl:#89b4fa"\
+" --color=fg:#585b70,header:#89b4fa,info:#f9e2af,pointer:#94e2d5"\
+" --color=marker:#94e2d5,fg+:#f5e0dc,prompt:#f9e2af,hl+:#89b4fa"
