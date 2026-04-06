58e8b55e89e543693b6384466505e68161aadb3a
phjorgensen <phjorgensenn@gmail.com>
2026-04-06 21:47:16 +0200

    Update to rewrite of nvim-treesitter

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/treesitter.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/treesitter.lua
@@ -1,36 +1,36 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-      ensure_installed = {
-        "markdown",
-        "markdown_inline",
-        "http",
-        "json",
-        "rust",
-        "go",
-        "html",
-        "css",
-        "scss",
-        "typescript",
-        "javascript",
-        "svelte",
-        "c",
-        "lua",
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-    })
-
-    vim.filetype.add({
-      pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+    require("nvim-treesitter").install({
+      "markdown",
+      "markdown_inline",
+      "http",
+      "json",
+      "rust",
+      "go",
+      "html",
+      "css",
+      "scss",
+      "typescript",
+      "javascript",
+      "svelte",
+      "c",
+      "lua",
     })
   end,
+  -- From old config, not yet moved over. Unsure how, and not sure if necessary.
+  -- configs.setup({
+  --   sync_install = false,
+  --   auto_install = true,
+  --   highlight = {
+  --     enable = true,
+  --     additional_vim_regex_highlighting = false,
+  --   },
+  -- })
+  -- vim.filetype.add({
+  --   pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+  -- })
 }
