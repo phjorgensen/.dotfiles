fb69c81adfa0c2b423ad5e93c45ea1af6d763de7
phjorgensen <phjorgensenn@gmail.com>
2026-04-10 21:32:05 +0200

    Update keybind for undotree

--- stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/undotree.lua
+++ stow_home/nvim/.config/nvim/lua/phjorgensen/plugins/undotree.lua
@@ -1,6 +1,6 @@
 return {
   "mbbill/undotree",
   config = function()
-    vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+    vim.keymap.set("n", "<leader>uu", vim.cmd.UndotreeToggle, { desc = "Open Undo Tree" })
   end,
 }
